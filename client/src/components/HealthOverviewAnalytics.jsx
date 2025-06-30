9bd07de03415967c3f647cc95d1453f4f7f4d19b
RakshithaNagaraju74 <161683357+RakshithaNagaraju74@users.noreply.github.com>
2025-06-30 23:58:11 +0530

    Update HealthOverviewAnalytics.jsx

--- client/src/components/HealthOverviewAnalytics.jsx
+++ client/src/components/HealthOverviewAnalytics.jsx
@@ -10,7 +10,7 @@ import {
 } from 'recharts';
 import clsx from 'clsx';
 
-const API_BASE_URL = 'http://localhost:5000'; // Replace if needed
+const API_BASE_URL = 'https://medwell-cv5p.onrender.com'; // Replace if needed
 
 export default function HealthOverviewAnalytics({ onBackClick }) {
   const [userEmail, setUserEmail] = useState(null);
